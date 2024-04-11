e8b3cde7624e4ed05a78e9a5dc03fa987dc28334
HjamesG <114678730+HjamesG@users.noreply.github.com>
2024-04-11 13:43:21 -0400

    Initial Commit

---
+++ GitDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Harrison Gilbert
+ * 
+ */
+public class GitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dr. Johnson! This is Harrison Gilbert.");
+
+	}
+
+}
